cf7dc5ac5bbca8edf44d17f82cf6bd5182d8c994
vosob <osobskyi.vlad@gmail.com>
2025-03-05 16:29:45 +0200

    add toolbar component

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,8 +1,11 @@
+import { ToolBar } from "./ToolBar/ToolBar";
+
 function App() {
   return (
     <>
       <div>
         <h1>Hello Vite + React!</h1>
+        <ToolBar />
       </div>
     </>
   );
---
+++ src/Components/ToolBar/ToolBar.jsx
@@ -0,0 +1,12 @@
+import css from "./ToolBar.module.css";
+
+export const ToolBar = () => {
+  return (
+    <div className={css.ToolBar}>
+      <h1>ToolBar</h1>
+      <h1>ToolBar</h1>
+      <h1>ToolBar</h1>
+      <h1>ToolBar</h1>
+    </div>
+  );
+};
