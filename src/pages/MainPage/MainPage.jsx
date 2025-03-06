760a85dedb2f967823c0696258ef8dd9db1140c4
Dima <dimavitchyk@gmail.com>
2025-03-06 22:25:30 +0200

    work

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,3 +1,8 @@
+import { Hero } from "../../Components/Hero/Hero";
 export const MainPage = () => {
-  return <div className="container"></div>;
+  return (
+    <div className="container">
+      <Hero />
+    </div>
+  );
 };
