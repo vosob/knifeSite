ee4fd3f4f3d89a37624acc51ff62055e76ffa1e6
vosob <osobskyi.vlad@gmail.com>
2025-03-07 22:21:21 +0200

    icon

--- src/Components/Hero/IconList/IconList.jsx
+++ src/Components/Hero/IconList/IconList.jsx
@@ -3,30 +3,36 @@ import { IoCheckmarkCircleOutline } from "react-icons/io5";
 import { LiaTruckMovingSolid } from "react-icons/lia";
 import { IoReorderThree } from "react-icons/io5";
 import { CiDiscount1 } from "react-icons/ci";
+
+const icons = [
+  {
+    icon: <IoCheckmarkCircleOutline />,
+    text: "Гарантія 100% повернення грошових коштів",
+  },
+  {
+    icon: <LiaTruckMovingSolid />,
+    text: "Доставка по Україні",
+  },
+  {
+    icon: <IoReorderThree />,
+    text: "Можливість оформлення замовлення без реєстрації",
+  },
+  {
+    icon: <CiDiscount1 />,
+    text: "Знижки постійним покупцям",
+  },
+];
+
 export const IconList = () => {
   return (
-    <div className={css.IconMain}>
-      <ul className={css.IconConteiner}>
-        <li className={css.iconItem}>
-          <IoCheckmarkCircleOutline size={35} />
-          <h1 className={css.IconTextWright}>
-            Гарантія 100% повернення грошових коштів
-          </h1>
-        </li>
-        <li className={css.iconItem}>
-          <LiaTruckMovingSolid size={35} />
-          <h1 className={css.IconTextWright}>Доставка по Україні</h1>
-        </li>
-        <li className={css.iconItem}>
-          <IoReorderThree size={35} />
-          <h1 className={css.IconTextWright}>
-            Можливість оформлення замовлення без реєстрації
-          </h1>
-        </li>
-        <li className={css.iconItem}>
-          <CiDiscount1 size={35} />
-          <h1 className={css.IconTextWright}>Знижки постійним покупцям</h1>
-        </li>
+    <div className={css.iconMain}>
+      <ul className={css.iconContainer}>
+        {icons.map(({ icon, text }, index) => (
+          <li key={index} className={css.iconListItem}>
+            <div className={css.icon}>{icon}</div>
+            <h3 className={css.iconText}>{text}</h3>
+          </li>
+        ))}
       </ul>
     </div>
   );
