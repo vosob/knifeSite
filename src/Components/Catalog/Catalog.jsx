8bc507cba3b496677397724ecab987115a0e1311
vosob <osobskyi.vlad@gmail.com>
2025-03-08 21:50:38 +0200

    add

--- src/Components/Catalog/Catalog.jsx
+++ src/Components/Catalog/Catalog.jsx
@@ -1,87 +1,97 @@
 import css from "./Catalog.module.css";
 export const Catalog = () => {
   return (
-    <div className={css.Maincatalog}>
+    <div className={css.MainCatalog}>
       <div className={css.CatalogContainer}>
-        <section>
+        <div>
           <h1>Каталог ножів</h1>
-          <ul>
+          <ul className={css.CatalogList}>
             <li>Роздільні</li>
             <li>Туристичні</li>
             <li>Мисливські</li>
           </ul>
-        </section>
-        <div className={css.LogoContainer}>
-          <img src="/public/img/catalog/knife-1493593_960_720 1.png" alt="" />
         </div>
-      </div>
-      <div className={css.CatalogContainer}>
-        <section>
-          <h1>Середньоклинкова зброя</h1>
-          <ul>
-            <li>Роздільні</li>
-            <li>Туристичні</li>
-            <li>Мисливські</li>
-          </ul>
-        </section>
-        <div className={css.LogoContainer}>
+        <div>
           <img
-            src="/public/img/catalog/dagger-png-transparent-picture 1.png"
+            className={css.Img}
+            src="/img/catalog/knife-1493593_960_720 1.png"
             alt=""
           />
         </div>
       </div>
       <div className={css.CatalogContainer}>
-        <section>
-          <h1>Довгоклинкова зброя</h1>
-          <ul>
+        <div>
+          <h1>Середньоклинкова зброя</h1>
+          <ul className={css.CatalogList}>
             <li>Роздільні</li>
             <li>Туристичні</li>
             <li>Мисливські</li>
           </ul>
-        </section>
-        <div className={css.LogoContainer}>
-          <img src="/public/img/catalog/holor048 1.png" alt="" />
         </div>
-      </div>
-      <div className={css.CatalogContainer}>
-        <section>
-          <h1>Сувенірні вироби</h1>
-          <ul>
-            <li>Роздільні</li>
-            <li>Туристичні</li>
-            <li>Мисливські</li>
-          </ul>
-        </section>
-        <div className={css.LogoContainer}>
-          <img src="/public/img/catalog/1029762 1.png" alt="" />
-        </div>
-      </div>
-      <div className={css.CatalogContainer}>
-        <section>
-          <h1>Супутні товари</h1>
-          <ul>
-            <li>Роздільні</li>
-            <li>Туристичні</li>
-            <li>Мисливські</li>
-          </ul>
-        </section>
-        <div className={css.LogoContainer}>
-          <img src="/public/img/catalog/kupit chehol 1.png" alt="" />
-        </div>
-      </div>
-      <div className={css.CatalogContainer}>
-        <section>
-          <h1>Ножова майстерня</h1>
-          <ul>
-            <li>Роздільні</li>
-            <li>Туристичні</li>
-            <li>Мисливські</li>
-          </ul>
-        </section>
-        <div className={css.LogoContainer}>
+        <div>
           <img
-            src="/public/img/catalog/damascus-steel-7379530_640.png"
+            className={css.Img}
+            src="/img/catalog/dagger-png-transparent-picture 1.png"
+            alt=""
+          />
+        </div>
+      </div>
+      <div className={css.CatalogContainer}>
+        <div>
+          <h1>Довгоклинкова зброя</h1>
+          <ul className={css.CatalogList}>
+            <li>Роздільні</li>
+            <li>Туристичні</li>
+            <li>Мисливські</li>
+          </ul>
+        </div>
+        <div>
+          <img className={css.Img} src="/img/catalog/holor048 1.png" alt="" />
+        </div>
+      </div>
+      <div className={css.CatalogContainer}>
+        <div>
+          <h1>Сувенірні вироби</h1>
+          <ul className={css.CatalogList}>
+            <li>Роздільні</li>
+            <li>Туристичні</li>
+            <li>Мисливські</li>
+          </ul>
+        </div>
+        <div>
+          <img className={css.Img} src="/img/catalog/1029762 1.png" alt="" />
+        </div>
+      </div>
+      <div className={css.CatalogContainer}>
+        <div>
+          <h1>Супутні товари</h1>
+          <ul className={css.CatalogList}>
+            <li>Роздільні</li>
+            <li>Туристичні</li>
+            <li>Мисливські</li>
+          </ul>
+        </div>
+        <div>
+          <img
+            className={css.Img}
+            src="/img/catalog/kupit chehol 1.png"
+            alt=""
+          />
+        </div>
+      </div>
+      <div className={css.CatalogContainer}>
+        <div>
+          <h1>Ножова майстерня</h1>
+          <ul className={css.CatalogList}>
+            <li>Роздільні</li>
+            <li>Туристичні</li>
+            <li>Мисливські</li>
+          </ul>
+        </div>
+        <div>
+          <img
+            className={css.Img}
+            src="/img/catalog/damascus-steel-7379530_640.png"
             alt=""
           />
         </div>
--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -2,6 +2,7 @@ import { Catalog } from "../../Components/Catalog/Catalog";
 import { IconList } from "../../Components/Hero/IconList/IconList";
 import { Hero } from "../../Components/Hero/Hero";
 import css from "./MainPage.module.css";
+import { TopSales } from "../../Components/TopSales/TopSales";
 export const MainPage = () => {
   return (
     <>
@@ -9,6 +10,7 @@ export const MainPage = () => {
         <Hero />
         <IconList />
         <Catalog />
+        <TopSales />
       </div>
     </>
   );
