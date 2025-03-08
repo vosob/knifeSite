0d3b2eb0f04d676f4443a30d316e97137c368745
vosob <osobskyi.vlad@gmail.com>
2025-03-08 13:41:19 +0200

    megamenu

--- src/Components/MegaMenu/MegaMenu.jsx
+++ src/Components/MegaMenu/MegaMenu.jsx
@@ -1,18 +1,42 @@
 import css from "./MegaMenu.module.css";
 
-const controls = [
-  "Каталог ножей",
-  "Клинковое оружие",
-  "Сувенирные изделия",
-  "Фонари ARMYTEK",
-  "Сопуствующие товары",
+const menuItems = [
+  {
+    title: "Каталог ножів",
+    catalog: ["Знижки", "Новинки", "Ножі", "Точила", "Мультитули"],
+  },
+  {
+    title: "Клинкова зброя",
+    catalog: ["Мечі", "Кинжали", "Шаблі", "Кортики"],
+  },
+  {
+    title: "Сувенірні вироби",
+    catalog: ["Подарункові набори", "Гравірування", "Статуетки"],
+  },
+  {
+    title: "Фонарі ARMYTEK",
+    catalog: ["Ліхтарі", "Аксесуари", "Зарядні пристрої"],
+  },
+  {
+    title: "Супутні товари",
+    catalog: ["Чохли", "Масла для догляду", "Серветки"],
+  },
 ];
 
 export const MegaMenu = () => {
   return (
     <div className={css.linksContainer}>
-      {controls.map((item) => (
-        <div className={css.links}>{item}</div>
+      {menuItems.map((menu, index) => (
+        <div key={index} className={css.linkWrapper}>
+          <div className={css.links}>{menu.title}</div>
+          <div className={css.dropdown}>
+            <ul>
+              {menu.catalog.map((item, idx) => (
+                <li key={idx}>{item}</li>
+              ))}
+            </ul>
+          </div>
+        </div>
       ))}
     </div>
   );
